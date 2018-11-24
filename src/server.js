1b333cf1c4f8cd99e98bba219a8316fad9f356eb
Mikel Princesa <mikelprincesa@gmail.com>
2018-11-24 11:53:57 -0500

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    fs = require('fs');
+
+var port = Number(process.env.PORT || 4000)
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(port)
+});
